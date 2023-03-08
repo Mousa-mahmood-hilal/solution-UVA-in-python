60f2c18a2011a5585abb03fc3c1195128b5d2f7b
Mousa mahmood <102973873+Mousa-hilal@users.noreply.github.com>
2023-03-08 16:21:13 +0300

    up

---
+++ root/Competitive Programming 3 The New Lower Bound of Programming Contests (Steven and Felix Halim)/Mathematics/Ad Hoc Mathematics Problems/The Simpler Ones/12149 - Feynman.py
@@ -0,0 +1,10 @@
+# user: Mousa-mahmood-hilal
+#Problem: UVA 12149 - Feynman
+while True:
+    n=int(input())
+    if n==0:
+        break
+    s=0
+    for i in range(1,n+1):
+        s+=i*i
+    print(s)
