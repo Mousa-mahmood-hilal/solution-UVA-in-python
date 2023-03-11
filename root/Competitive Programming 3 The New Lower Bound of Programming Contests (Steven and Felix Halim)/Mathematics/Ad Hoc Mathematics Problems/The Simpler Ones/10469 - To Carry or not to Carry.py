c0ed5fb2df300ce388a56fbc7c02330f48ad6cdb
Mousa mahmood <102973873+Mousa-hilal@users.noreply.github.com>
2023-03-11 21:07:08 +0300

    solution 10469 - To Carry or not to Carry

---
+++ root/Competitive Programming 3 The New Lower Bound of Programming Contests (Steven and Felix Halim)/Mathematics/Ad Hoc Mathematics Problems/The Simpler Ones/10469 - To Carry or not to Carry.py
@@ -0,0 +1,8 @@
+# user: Mousa-mahmood-hilal
+#Problem: UVA 10469 - To Carry or not to Carry
+while True:
+    try:
+        n,m=map(int,input().split())
+        print(n^m)
+    except:
+        break
